c6fd9856b6089ec7dd3128d3f61124137572cdce
Laterner <ivantrofimov2001@gmail.com>
2024-06-12 23:31:33 +0300

    1

--- vkr/models.py
+++ vkr/models.py
@@ -51,7 +51,7 @@ class Event (models.Model):
 
 class EventMembers(models.Model):
     id = models.IntegerField(primary_key=True)
-    event = models.IntegerField(default=1,blank=True, null=True)
+    event = models.IntegerField(default=1, blank=True, null=True)
     enjoy_date = models.DateTimeField(default=timezone.now)
     
     user_prof = models.ForeignKey(
